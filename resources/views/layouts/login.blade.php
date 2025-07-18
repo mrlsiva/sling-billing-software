bdb7e7a61657fae5910f789faba2b4a09e4e0085
siva <pmrlsivas@gmail.com>
2025-07-18 21:49:00 +0530

    login UI

--- resources/views/layouts/login.blade.php
+++ resources/views/layouts/login.blade.php
@@ -7,7 +7,7 @@
 	@yield('style')
 	 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
 	<link rel="stylesheet" href="{{ asset('assets/css/app.css') }}">
-	<link rel="stylesheet" href="{{asset('assets/css/style.css')}}">
+	<link rel="stylesheet" href="{{asset('assets/css/login.css')}}">
 </head>
 <body>
 	@yield('body')
