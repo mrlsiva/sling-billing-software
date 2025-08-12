e92f72f3d22661671f14835b6e5a1d942e0f9e48
siva <pmrlsivas@gmail.com>
2025-08-12 19:32:42 +0530

    dashboard changes

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -11,16 +11,24 @@
                 @foreach($shops as $shop)
                     <div class="col-md-3 col-md-3">
                         <div class="card">
-                                <div class="card-body">
+                                <div class="card-body pt-3 pb-2 px-4">
                                     <div class="d-flex align-items-center justify-content-between">
                                         <div>
                                             <p class="mb-3 card-title">{{$shop->name}}</p>
-                                            <h4 class="fw-bold d-flex align-items-center gap-2 mb-0">30 Orders</h4>
+                                            <h4 class="fw-bold d-flex align-items-center gap-2 mb-0">3 <span class="fs-10 mt-2"> Branches</span></h4>
                                         </div>
-                                        <div>
+                                        <div class="d-flex flex-column align-items-center justify-content-center gap-2">
+                                            <div class="d-flex align-items-center gap-2 pt-2">
+                                                <div class="box" style="background-color: #000;"></div>
+                                                <div class="box" style="background-color: #000;"></div>
+                                            </div>
                                             <a href="{{route('admin.shop.view', ['id' => $shop->id])}}"> <i class="ri-arrow-right-circle-line fs-32 text-muted"></i></a>
                                         </div>
                                     </div>
+                                </div>
+                                <div class="card-Footer border px-4 pt-2 d-flex justify-content-between align-items-center">
+                                    <h4 class="fw-bold d-flex align-items-center gap-2">10 <span class="fs-10 mt-2"> Categories</span></h4>
+                                    <h4 class="fw-bold d-flex align-items-center gap-2 ">29 <span class="fs-10 mt-2"> Products</span></h4>
                                 </div>
                         </div>
                     </div>
