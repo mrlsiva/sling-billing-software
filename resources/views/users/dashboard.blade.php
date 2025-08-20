19391391fd40eac60e5a465996b7c4c44bc1417b
siva <pmrlsivas@gmail.com>
2025-08-20 14:33:27 +0530

    Ho dashboard

--- resources/views/users/dashboard.blade.php
+++ resources/views/users/dashboard.blade.php
@@ -21,13 +21,77 @@
 <div class="row">
 	<div class="col-xl-12 col-md-12">
 		<div class="row">
-			@foreach($branches as $branch)
 			<div class="col-md-3 col-md-3">
 				<div class="card">
 					<div class="card-body">
 						<div class="d-flex align-items-center justify-content-between">
 							<div>
-								<p class="mb-3 card-title">{{$branch->name}} - {{$branch->user_name}}</p>
+								<p class="mb-3 card-title">No.Of Branches </p>
+								@foreach($branches as $branch)
+								<h4 class="fw-bold d-flex align-items-center gap-2 mb-0"> {{ count($branches) }}</h4>
+								@endforeach
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="col-md-3 col-md-3">
+				<div class="card">
+					<div class="card-body">
+						<div class="d-flex align-items-center justify-content-between">
+							<div>
+								<p class="mb-3 card-title">No.Of Categories </p>
+
+								<h4 class="fw-bold d-flex align-items-center gap-2 mb-0"> 2</h4>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="col-md-3 col-md-3">
+				<div class="card">
+					<div class="card-body">
+						<div class="d-flex align-items-center justify-content-between">
+							<div>
+								<p class="mb-3 card-title">No. Of Sub Categories</p>
+								<h4 class="fw-bold d-flex align-items-center gap-2 mb-0">
+									5
+								</h4>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<div class="col-md-3 col-md-3">
+				<div class="card">
+					<div class="card-body">
+						<div class="d-flex align-items-center justify-content-between">
+							<div>
+								<p class="mb-3 card-title">No.Of Products </p>
+								<h4 class="fw-bold d-flex align-items-center gap-2 mb-0"> 23</h4>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+
+		</div>
+	</div>
+	<div class="col-xl-12 col-md-12">
+		<div class="row">
+			<div class="col-md-12 col-md-12 mb-3">
+				Branch List
+			</div>
+			@foreach($branches as $branch)
+
+			<div class="col-md-3 col-md-3">
+				<div class="card">
+					<div class="card-body">
+						<div class="d-flex align-items-center justify-content-between">
+							<div>
+								<!-- {{$branch->name}} -  -->
+								<p class="mb-3 card-title">{{$branch->user_name}}</p>
 								<h4 class="fw-bold d-flex align-items-center gap-2 mb-0">0 Orders</h4>
 							</div>
 							<div>
