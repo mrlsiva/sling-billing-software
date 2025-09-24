5a599e02073eb226ab1afc947f9fd0bb8cab596b
siva <pmrlsivas@gmail.com>
2025-09-24 11:16:44 +0530

    css qr

--- resources/views/branches/products/barcode.blade.php
+++ resources/views/branches/products/barcode.blade.php
@@ -8,73 +8,72 @@
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 	<style>
 		.qr-container {
-			    background: gray;
-    display: flex ;
-    flex-wrap: wrap;
-    width: 435px;
+			background: gray;
+			display: flex ;
+			flex-wrap: wrap;
+			width: 435px;
 		}
 
 		.qr-item {
 			margin: 1px;
-    width: 132.28346457px;
-    height: 83.149606299px;
-    overflow: hidden;
-    background-color: #f6f6f6;
+			width: 132.28346457px;
+			height: 83.149606299px;
+			overflow: hidden;
+			background-color: #f6f6f6;
 		}
 		.qr-item p{
-			    width: calc(100% - 10px);
-    font-size: 10px;
-    padding: 5px;
-    font-weight: bold;
-    white-space: nowrap;
-    overflow: hidden;
-    text-overflow: ellipsis;
-		}
-		
-		.shop-title {
-			    width: calc(100% - 10px);
-    font-size: 10px;
-    padding: 5px;
-    font-weight: bold;
-    white-space: nowrap;
-    overflow: hidden;
-    text-overflow: ellipsis;
-		}
-		.qr-body {
-			    width: 100%;
-    padding: 2px;
-		}
-		.qr-left {
-			    width: 42px;
-    margin-right: 2px;
-    float: left;
-    text-align: center;
-		}
-		.qr-left img {
-			       width: 42px;
-    height: 42px;
+			width: calc(100% - 10px);
+			font-size: 10px;
+			padding: 5px;
+			font-weight: bold;
+			white-space: nowrap;
+			overflow: hidden;
+			text-overflow: ellipsis;
 		}
 
+		.shop-title {
+			width: calc(100% - 10px);
+			font-size: 10px;
+			padding: 5px;
+			font-weight: bold;
+			white-space: nowrap;
+			overflow: hidden;
+			text-overflow: ellipsis;
+		}
+		.qr-body {
+			width: 100%;
+			padding: 2px;
+		}
+		.qr-left {
+			width: 42px;
+			margin-right: 2px;
+			float: left;
+			text-align: center;
+		}
+		.qr-left img, .qr-left svg {
+			width: 42px;
+			height: 42px;
+		}
 		.qr-right {
-			    width: calc(100% - 49px);
-    display: inline-block;
-    padding-left: 5px;
+			width: calc(100% - 49px);
+			display: inline-block;
+			padding-left: 5px;
 		}
 
 		.qr-right p.name {
-			    width: 100%;
-    height: 24px;
-    min-height: 24px;
-    max-height: 24px;
-    overflow: hidden;
-    font-size: 10px;
-    line-height: 12px;
+			width: 100%;
+			height: 24px;
+			min-height: 24px;
+			max-height: 24px;
+			overflow: hidden;
+			font-size: 10px;
+			line-height: 12px;
 		}
 		.qr-right p.price {
-			    font-size: 14px;
-    width: 100%;
-    font-weight: bold;
-    margin-top: 5px;
+			font-size: 14px;
+			width: 100%;
+			font-weight: bold;
+			margin-top: 5px;
 		}
 
 		@media print {
