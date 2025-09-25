38ca0b7550f69c573bb2f2fa2afae871bbb660c9
siva <pmrlsivas@gmail.com>
2025-09-25 17:15:46 +0530

    UI

--- resources/views/branches/dashboard.blade.php
+++ resources/views/branches/dashboard.blade.php
@@ -12,9 +12,75 @@
 			<div class="card-header d-flex justify-content-between align-items-center">
 				<div>
 					<p class="card-title">Welcome {{Auth::user()->user_name}} - Branch</p>
+					
 				</div>
 			</div>
 		</div>
 	</div>
+</div>
+<div class="row">
+	<div class="col-xl-12 col-md-12">
+		<div class="row">
+						 
+			<div class="col-md-3 col-md-3">
+				<div class="card">
+					<div class="card-body">
+						<div class="d-flex align-items-center justify-content-between">
+							<div>
+								<p class="mb-3 card-title">Today Order </p>
+
+								<h4 class="fw-bold d-flex align-items-center gap-2 mb-0"> 5</h4>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="col-md-3 col-md-3">
+				<div class="card">
+					<div class="card-body">
+						<div class="d-flex align-items-center justify-content-between">
+							<div>
+								<p class="mb-3 card-title">Total Order</p>
+								<h4 class="fw-bold d-flex align-items-center gap-2 mb-0">
+									10
+								</h4>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<div class="col-md-3 col-md-3">
+				<div class="card">
+					<div class="card-body">
+						<div class="d-flex align-items-center justify-content-between">
+							<div>
+								<p class="mb-3 card-title">Today Sales </p>
+								<h4 class="fw-bold d-flex align-items-center gap-2 mb-0"> 1000</h4>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="col-md-3 col-md-3">
+				<div class="card">
+					<div class="card-body">
+						
+						<div class="d-flex align-items-center justify-content-between">
+							<div>
+								<!-- Vasantham -  -->
+								<p class="mb-3 card-title">vasantham-valasai</p>
+								<h4 class="fw-bold d-flex align-items-center gap-2 mb-0">3 Orders</h4>
+							</div>
+							<div>
+								<a href="#!"> <i class="ri-arrow-right-circle-line fs-32 text-muted"></i></a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	
 </div>
 @endsection
--- resources/views/users/settings/general.blade.php
+++ resources/views/users/settings/general.blade.php
@@ -1,1 +1,190 @@
-general.blade.php
+@extends('layouts.master')
+
+@section('title')
+<title>{{ config('app.name')}} | Finance</title>
+@endsection
+
+@section('body')
+    <div class="row">
+		<div class="col-xl-12">
+			<div class="card">
+				<div class="card-header d-flex justify-content-between align-items-center">
+					<div>
+						<p class="card-title mb-0">General Settings</p>
+					</div>
+				</div>				
+			</div>
+		</div>
+	</div>
+    <div class="row">
+        <div class="col-lg-12">
+            <div class="card">
+                <div class="card-header">
+                    <h4 class="card-title mb-0">Payment Method Settings</h4>
+                </div>
+                <div class="card-body">
+                    <div class="row">
+                            <div class="col-lg-3">
+                                <div class="form-group mb-3">
+                                    <p class="fw-medium mb-2">Cash</p>
+                                    <div class="form-check form-switch">
+                                        <input class="form-check-input" type="checkbox" id="customersOnline" checked="">
+                                        <label class="form-check-label" for="customersOnline">Yes</label>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-lg-3">
+                                <div class="form-group mb-3">
+                                    <p class="fw-medium mb-2">Credit</p>
+                                    <div class="form-check form-switch">
+                                        <input class="form-check-input" type="checkbox" id="customersActivity" checked="">
+                                        <label class="form-check-label" for="customersActivity">Yes</label>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-lg-3">
+                                <div class="form-group mb-3">
+                                    <p class="fw-medium mb-2">UPI</p>
+                                    <div class="form-check form-switch">
+                                        <input class="form-check-input" type="checkbox" id="customerSearches" checked="">
+                                        <label class="form-check-label" for="customerSearches">Yes</label>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-lg-3">
+                                <div class="form-group">
+                                    <p class="fw-medium mb-2">Card</p>
+                                    <div class="form-check form-switch">
+                                        <input class="form-check-input" type="checkbox" id="guestCheckout">
+                                        <label class="form-check-label" for="guestCheckout">Yes</label>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-lg-3">
+                                <div class="form-group">
+                                    <p class="fw-medium mb-2">Finance</p>
+                                    <div class="form-check form-switch">
+                                        <input class="form-check-input" type="checkbox" id="loginDisplayPrice">
+                                        <label class="form-check-label" for="loginDisplayPrice">Yes</label>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-lg-3">
+                                <div class="form-group">
+                                    <p class="fw-medium mb-2">Finance</p>
+                                    <div class="form-check form-switch">
+                                        <input class="form-check-input" type="checkbox" id="loginDisplayPrice">
+                                        <label class="form-check-label" for="loginDisplayPrice">Yes</label>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-lg-3">
+                                <div class="form-group">
+                                    <p class="fw-medium mb-2">Finance</p>
+                                    <div class="form-check form-switch">
+                                        <input class="form-check-input" type="checkbox" id="loginDisplayPrice">
+                                        <label class="form-check-label" for="loginDisplayPrice">Yes</label>
+                                    </div>
+                                </div>
+                            </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-3">
+            <div class="card">
+                <div class="card-header">
+                    <h4 class="card-title mb-0">Categories Count</h4>
+                </div>
+                <div class="card-body">
+                    <div class="form-group mb-3">
+                            <!-- <p class="fw-medium mb-2"></p> -->
+                            <div class="form-check form-switch">
+                                <input class="form-check-input" type="checkbox" id="categoryProductCount" checked="">
+                                <label class="form-check-label" for="categoryProductCount">Default</label>
+                            </div>
+                    </div>
+                    <div class="form-group">
+                            <form>
+                                <div class="">
+                                    <label for="items-par-page" class="form-label">Default Items Per Page</label>
+                                    <input type="number" id="items-par-page" class="form-control" placeholder="000">
+                                </div>
+                            </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-3">
+            <div class="card">
+                <div class="card-header">
+                    <h4 class="card-title mb-0">Sub Categories Page</h4>
+                </div>
+                <div class="card-body">
+                    <div class="form-group mb-3">
+                            <!-- <p class="fw-medium mb-2"></p> -->
+                            <div class="form-check form-switch">
+                                <input class="form-check-input" type="checkbox" id="categoryProductCount" checked="">
+                                <label class="form-check-label" for="categoryProductCount">Default</label>
+                            </div>
+                    </div>
+                    <div class="form-group">
+                            <form>
+                                <div class="">
+                                    <label for="items-par-page" class="form-label">Default Items Per Page</label>
+                                    <input type="number" id="items-par-page" class="form-control" placeholder="000">
+                                </div>
+                            </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-3">
+            <div class="card">
+                <div class="card-header">
+                    <h4 class="card-title mb-0">Product Page</h4>
+                </div>
+                <div class="card-body">
+                    <div class="form-group mb-3">
+                            <!-- <p class="fw-medium mb-2"></p> -->
+                            <div class="form-check form-switch">
+                                <input class="form-check-input" type="checkbox" id="categoryProductCount" checked="">
+                                <label class="form-check-label" for="categoryProductCount">Default</label>
+                            </div>
+                    </div>
+                    <div class="form-group">
+                            <form>
+                                <div class="">
+                                    <label for="items-par-page" class="form-label">Default Items Per Page</label>
+                                    <input type="number" id="items-par-page" class="form-control" placeholder="000">
+                                </div>
+                            </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-3">
+            <div class="card">
+                <div class="card-header">
+                    <h4 class="card-title mb-0">Reviews Settings</h4>
+                </div>
+                <div class="card-body">
+                    <div class="form-group mb-3">
+                            <p class="fw-medium mb-2">Allow Reviews</p>
+                            <div class="form-check form-switch">
+                                <input class="form-check-input" type="checkbox" id="allowReviews" checked="">
+                                <label class="form-check-label" for="allowReviews">Yes</label>
+                            </div>
+                    </div>
+                    <div class="form-group">
+                            <p class="fw-medium mb-2">Allow Guest Reviews</p>
+                            <div class="form-check form-switch">
+                                <input class="form-check-input" type="checkbox" id="gaustReviews">
+                                <label class="form-check-label" for="gaustReviews">Yes</label>
+                            </div>
+                    </div>
+                </div>
+            </div>
+        </div>            
+</div>
+@endsection
