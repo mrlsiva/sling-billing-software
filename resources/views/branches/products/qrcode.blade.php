de21931daa1b8baa3d7e7cbc9583c6b78e3057ca
siva <pmrlsivas@gmail.com>
2025-09-25 15:43:51 +0530

    qr

--- resources/views/branches/products/qrcode.blade.php
+++ resources/views/branches/products/qrcode.blade.php
@@ -77,6 +77,14 @@
 		}
 
 		@media print {
+			* {
+                transition: none !important;
+            }
+
+            @page {
+                size: 4.13in;
+               size:portrait;
+            }
 			body {
 				background: #fff !important;
 			}
@@ -86,6 +94,74 @@
 			.qr-item {
 				page-break-inside: avoid;
 			}
+			.qr-container {
+			background: gray;
+			display: flex ;
+			flex-wrap: wrap;
+			width: 435px;
+		}
+
+		.qr-item {
+			margin: 1px;
+			width: 132.28346457px;
+			height: 83.149606299px;
+			overflow: hidden;
+			background-color: #f6f6f6;
+		}
+		.qr-item p{
+			width: calc(100% - 10px);
+			font-size: 10px;
+			padding: 5px;
+			font-weight: bold;
+			white-space: nowrap;
+			overflow: hidden;
+			text-overflow: ellipsis;
+		}
+
+		.shop-title {
+			width: calc(100% - 10px);
+			font-size: 10px;
+			padding: 5px;
+			font-weight: bold;
+			white-space: nowrap;
+			overflow: hidden;
+			text-overflow: ellipsis;
+		}
+		.qr-body {
+			width: 100%;
+			padding: 2px;
+		}
+		.qr-left {
+			width: 42px;
+			margin-right: 2px;
+			float: left;
+			text-align: center;
+		}
+		.qr-left img, .qr-left svg {
+			width: 42px;
+			height: 42px;
+		}
+		.qr-right {
+			width: calc(100% - 49px);
+			display: inline-block;
+			padding-left: 5px;
+		}
+
+		.qr-right .name {
+			width: 100%;
+			height: 24px;
+			min-height: 24px;
+			max-height: 24px;
+			overflow: hidden;
+			font-size: 10px;
+			line-height: 12px;
+		}
+		.qr-right .price {
+			font-size: 14px;
+			width: 100%;
+			font-weight: bold;
+			margin-top: 5px;
+		}
 		}
 	</style>
 </head>
