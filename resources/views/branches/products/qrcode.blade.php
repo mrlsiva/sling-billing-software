ecd8a7d0c35cc84e18ae59ba43e12df4e8881465
siva <pmrlsivas@gmail.com>
2025-09-26 16:09:16 +0530

    qr

--- resources/views/branches/products/qrcode.blade.php
+++ resources/views/branches/products/qrcode.blade.php
@@ -8,27 +8,7 @@
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 	<style>
 		
-
-		@media print {
-			* {
-                transition: none !important;
-            }
-
-            @page {
-                size: 4.13in;
-               size:portrait;
-            }
-			body {
-				background: #fff !important;
-				height: 83.149606299px;
-			}
-			.qr-container {
-				gap: 10px;
-			}
-			.qr-item {
-				page-break-inside: avoid;
-			}
-			.qr-container {
+.qr-container {
 			background: gray;
 			display: flex ;
 			flex-wrap: wrap;
@@ -96,6 +76,25 @@
 			font-weight: bold;
 			margin-top: 5px;
 		}
+		@media print {
+			* {
+                transition: none !important;
+            }
+
+            @page {
+                size: 4.13in;
+            }
+			body {
+				background: #fff !important;
+				height: 83.149606299px;
+			}
+			.qr-container {
+				gap: 10px;
+			}
+			.qr-item {
+				page-break-inside: avoid;
+			}
+			
 		}
 	</style>
 </head>
