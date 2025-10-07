fb34fbd80be61eb9988afd9dd2a8f36db9e2c6dd
tena <tena.visansoft@gmail.com>
2025-10-07 10:40:13 +0530

    same

--- resources/views/layouts/landing.blade.php
+++ resources/views/layouts/landing.blade.php
@@ -4,17 +4,24 @@
 		<meta charset="utf-8">
 		@yield('title')
 		@yield('style')
+
+		@php
+        	$user = App\Models\User::where('slug_name',request()->segment(1))->first();
+    	@endphp
+
 		<meta name="viewport" content="width=device-width, initial-scale=1" />
 		<meta name="description" content="A fully responsive premium Bootstrap admin dashboard template for modern web applications." />
 		<meta name="author" content="FoxPixel" />
 		<meta http-equiv="X-UA-Compatible" content="IE=edge" />
 		<meta charset="utf-8">
 		<!-- Bootstrap 5 CSS -->
+		<link rel="icon" type="image/png" href="{{ asset('storage/' . $user->fav_icon) }}">
 		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
 		<!-- Bootstrap Icons -->
 		<link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css" rel="stylesheet">
 		<link rel="stylesheet" href="{{asset('assets/css/landing.css')}}">
 		<link rel="stylesheet" href="{{ asset('assets/css/app.css') }}">
+
     </head>
 	<body>
 		@yield('body')
