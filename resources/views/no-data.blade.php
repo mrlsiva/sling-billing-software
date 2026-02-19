c6cb902a40381b64bf800f33b99cc68e1ab135e6
Francis <francis.francis20.1989@gmail.com>
2026-02-19 11:27:17 +0530

    no data

--- resources/views/no-data.blade.php
+++ resources/views/no-data.blade.php
@@ -1,1 +1,3 @@
-no-data.blade.php
+<div syle="display: flex; justify-content: center; align-items: center; height: 100vh;">
+    <h1 style="font-size: 24px; color: #555;">No data available</h1>
+</div>
