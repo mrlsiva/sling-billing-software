8a850ea8db41c33d076663669dc1c3b237ff37b4
siva <pmrlsivas@gmail.com>
2026-04-08 17:46:06 +0530

    web

--- routes/web.php
+++ routes/web.php
@@ -149,6 +149,7 @@ else
                             Route::get('/get_sub_category',[productController::class, 'get_sub_category'])->name('get_sub_category');
                             Route::get('/download',[productController::class, 'download'])->name('download');
                             Route::post('/bulk_upload',[productController::class, 'bulk_upload'])->name('bulk_upload');
+                            Route::get('/bulk_upload', fn() => redirect()->route('product.index', ['company' => request()->route('company')]))->name('bulk_upload.get');
                             Route::get('{product}/detail',[productController::class, 'detail'])->name('detail');
                             
                         });
