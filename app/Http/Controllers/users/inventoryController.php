c3c49be975e358c529a5facaab096a92c18d5b2c
siva <pmrlsivas@gmail.com>
2026-05-10 20:17:59 +0530

    stock download issue fixes

--- app/Http/Controllers/users/inventoryController.php
+++ app/Http/Controllers/users/inventoryController.php
@@ -84,6 +84,9 @@ class inventoryController extends Controller
 
     public function download(Request $request, $company, $shop, $branch)
     {
+        set_time_limit(300);
+        ini_set('memory_limit', '512M');
+
         $query = Stock::where('shop_id', $shop);
 
         if ($branch != 0) {
